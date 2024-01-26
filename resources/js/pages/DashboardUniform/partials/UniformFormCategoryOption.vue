18ca5ef69799826a7a392439efa047da1c818a6c
João Antônio Hamerski Copetti <joaohamerski@outlook.com>
2024-01-26 17:20:18 -0300

    Update

---
+++ resources/js/pages/DashboardUniform/partials/UniformFormCategoryOption.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { Category } from '@/types/pages'
+
+defineProps<{ item: Category }>()
+</script>
+<template>
+  <div class="flex items-center gap-4">
+    <div class="w-12">
+      <img
+        class="rounded max-h-20"
+        :src="item.image.url"
+        alt=""
+      />
+    </div>
+    <div>
+      {{ item.name }}
+    </div>
+  </div>
+</template>
