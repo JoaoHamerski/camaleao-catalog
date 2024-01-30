cae7e5da6b300bcf5afe19162e89261d3b977a97
João Antônio Hamerski Copetti <joaohamerski@hotmail.com>
2024-01-30 15:39:22 -0300

    Update

--- domains/Category/Controllers/CategoryShowController.php
+++ domains/Category/Controllers/CategoryShowController.php
@@ -5,6 +5,7 @@ namespace Domains\Category\Controllers;
 use App\Http\Controllers\Controller;
 use Domains\Category\Models\Category;
 use Domains\DashboardUniform\Resources\UniformResource;
+use Illuminate\Support\Facades\Storage;
 use Inertia\Inertia;
 
 class CategoryShowController extends Controller
@@ -17,6 +18,11 @@ class CategoryShowController extends Controller
             ->paginate(12, ['*'], 'pagina');
 
         return Inertia::render('Categories/TheCategoriesShow', [
+            'meta' => [
+                'title' => $category->name,
+                'description' => $category->name,
+                'image' => url(Storage::url('categorias/' . $category->image['filename']))
+            ],
             'category' => $category,
             'uniforms' => UniformResource::collection($uniforms)
         ]);
