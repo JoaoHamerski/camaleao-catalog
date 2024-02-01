cb889ec48a4dc206bc864dc410f1eafcb22744e8
João Antônio Hamerski Copetti <joaohamerski@hotmail.com>
2024-02-01 18:26:00 -0300

    Update

--- resources/js/bootstrap/bootstrap-page.ts
+++ resources/js/bootstrap/bootstrap-page.ts
@@ -2,6 +2,8 @@ import MainLayout from '@/pages/Common/layouts/MainLayout.vue'
 import { DefineComponent } from 'vue'
 
 export default class BootstrapPage {
+  protected static APP_NAME = 'Camaleão Catálogo'
+
   protected static definePageLayout(page: DefineComponent) {
     page.default.layout = page.default.layout || MainLayout
   }
@@ -12,11 +14,17 @@ export default class BootstrapPage {
     return pages[`../pages/${name}.vue`]()
   }
 
-  public static async resolvePageComponent(name: string) {
+  public static async resolveComponent(name: string) {
     const page = await BootstrapPage.getRequestedPage(name)
 
     BootstrapPage.definePageLayout(page)
 
     return page
+  }
+
+  public static defineTitle(title: string) {
+    return title
+      ? `${title} | ${BootstrapPage.APP_NAME}`
+      : BootstrapPage.APP_NAME
   }
 }
